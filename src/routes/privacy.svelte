41706ab615a7e3a6713cda63823dadd2fbb7a28f
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-03-13 20:42:45 +0000

    Add analytics info to privacy page
    Closes #17

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -2,4 +2,10 @@
 	<title>Privacy</title>
 </svelte:head>
 
-<p>We will delete all your data automatically after 30 days (so make sure you finish organising your pissup within that time!)</p>
+<h2>Data we keep for 30 days</h2>
+<p>We delete this data automatically after 30 days (so make sure you finish organising your pissup within that time!)</p>
+<p>Examples: Name, available dates, date of pissup.</p>
+
+<h2>Data we keep indefinitely</h2>
+<p>We collect anonymous statistics about our visitors to understand how the product is being used. No personally identifiable data is collected.</p>
+<p>Examples: How many people have visited a particular page, how many people clicked a particular button.</p>
