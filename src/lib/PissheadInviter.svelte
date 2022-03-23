e4a14764d9724b4cf6dca390f9c41b1a021a7c07
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-03-23 21:03:55 +0000

    Fix Safari error after cancelling share

--- src/lib/PissheadInviter.svelte
+++ src/lib/PissheadInviter.svelte
@@ -12,6 +12,7 @@
 			})
 		} catch (error) {
 			console.warn('Unable to use native sharing', error)
+			if (error.name === 'AbortError') return // If we try to fallback to 'copy' after this it results in a NotAllowedErorr. I'm guesing this is because when the user aborts share it resets Safari's concept of if is a valid and safe clipboard operation triggered by a user interaction (see https://webkit.org/blog/10247/new-webkit-features-in-safari-13-1/).
 			copy()
 		}
 	}
