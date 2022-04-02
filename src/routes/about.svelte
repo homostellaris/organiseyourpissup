4c5999921fec85f49ace85643d203595829b5ceb
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-04-02 21:55:58 +0100

    Provide more explanation on about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -18,6 +18,9 @@
 	<title>About</title>
 </svelte:head>
 
-<p>Busy? Thirsty? Mates impossible to organise? Then this is the tool for you. Find a day that works for everyone in minutes.</p>
+<h1>The painless way to agree a date with your mates</h1>
+<p>Busy? Thirsty? Mates impossible to organise? Then <strong>Organise Your Pissup</strong> can help.</p>
+<p>Trying to find a date when everyone is available for a few drinks is a painful process, especially over text. We save you time by collecting everyone's availability and presenting you with the best dates with a minimum of fuss.</p>
 
-<p>This app was created out of necessity when the author realised his mates were nigh on impossible to organise.</p>
+<a id="product-hunt-featured-count" href="https://www.producthunt.com/posts/organise-your-pissup?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-organise&#0045;your&#0045;pissup" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=337271&theme=light" alt="Organise&#0032;Your&#0032;Pissup - The&#0032;painless&#0032;way&#0032;to&#0032;agree&#0032;a&#0032;date&#0032;with&#0032;your&#0032;mates | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+<a href="https://www.producthunt.com/posts/organise-your-pissup?utm_source=badge-review&utm_medium=badge&utm_souce=badge-organise&#0045;your&#0045;pissup#discussion-body" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/review.svg?post_id=337271&theme=light" alt="Organise&#0032;Your&#0032;Pissup - The&#0032;painless&#0032;way&#0032;to&#0032;agree&#0032;a&#0032;date&#0032;with&#0032;your&#0032;mates | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
